dcf2f75de56bda1fc6864329c1e3b378d17440ed
Keith Stateson <keithstateson@gmail.com>
2024-07-02 06:46:49 -0700

    clean up and updates

--- Lab5_3_Function_Find_Min_Value/main.py
+++ Lab5_3_Function_Find_Min_Value/main.py
@@ -5,30 +5,35 @@ def mineven(numbers):
     """
     Determine the minimum even number in a list, extract it,
     and print the min even number and the updated list.
+    
+    In Python, lists are mutable objects, and when you pass a list to a function,
+    you are passing a reference to that list. This means that any modifications to the list
+    within the function will affect the original list outside the function.
+    You don't need to pass the list back out of the function because
+    the changes are made directly to the original list.
     """
+    
     even_list = []
     for num in numbers:
         if num % 2 == 0:
             even_list.append(num)
-    minval = min(even_list)
-
-    if even_list:
-        return minval
-    else:
-        return None
+    minval = min(even_list)       
+    numbers.remove(minval)
+    
+    return minval
 
 
 def main():
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     result = mineven(numbers)
-    numbers.remove(result)
+    # numbers.remove(result)
     # del numbers[numbers.index(result)]
     print(numbers)
     print(result)
 
     numbers = [1, 3, 5, 7, 10, 14, 2, 8]
     result = mineven(numbers)
-    numbers.remove(result)
+    # numbers.remove(result)
     # del numbers[numbers.index(result)]
     print(numbers)
     print(result)
@@ -37,7 +42,7 @@ def main():
     numbers = [random.randint(-50, 50) for v in range(N)]
     print(numbers)
     result = mineven(numbers)
-    numbers.remove(result)
+    # numbers.remove(result)
     # del numbers[numbers.index(result)]
     print(numbers)
     print(result)
