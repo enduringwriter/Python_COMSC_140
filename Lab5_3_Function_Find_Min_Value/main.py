1c69817eddfb64be18e4ebb7741fc823e6b098cb
Keith Stateson <keithstateson@gmail.com>
2024-07-01 21:32:03 -0700

    update

--- Lab5_3_Function_Find_Min_Value/main.py
+++ Lab5_3_Function_Find_Min_Value/main.py
@@ -1,29 +1,35 @@
 import random
 
+
 def mineven(numbers):
     """
     Determine the minimum even number in a list, extract it,
     and print the min even number and the updated list.
     """
-
     even_list = []
-    for _ in numbers:
-        if _ % 2 == 0:
-            even_list.append(_)
-            
+    for num in numbers:
+        if num % 2 == 0:
+            even_list.append(num)
     minval = min(even_list)
 
-    return minval
+    if even_list:
+        return minval
+    else:
+        return None
 
 
 def main():
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     result = mineven(numbers)
+    numbers.remove(result)
+    # del numbers[numbers.index(result)]
     print(numbers)
     print(result)
 
     numbers = [1, 3, 5, 7, 10, 14, 2, 8]
     result = mineven(numbers)
+    numbers.remove(result)
+    # del numbers[numbers.index(result)]
     print(numbers)
     print(result)
 
@@ -31,6 +37,8 @@ def main():
     numbers = [random.randint(-50, 50) for v in range(N)]
     print(numbers)
     result = mineven(numbers)
+    numbers.remove(result)
+    # del numbers[numbers.index(result)]
     print(numbers)
     print(result)
 
